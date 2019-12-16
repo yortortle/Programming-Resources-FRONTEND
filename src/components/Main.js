b34351dc0c266ee994052bf04b296d99314ac79c
Troy Hancock <Troy@Troys-MacBook-Pro.local>
2019-12-16 13:58:47 -0500

    safd

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,7 +17,7 @@ let baseUrl = '';
 if (process.env.NODE_ENV === 'development') {
   baseUrl = 'http://cors-anywhere.herokuapp.com/https://serene-atoll-56752.herokuapp.com'
 } else {
-  console.log('this is for heroku');
+  baseUrl = 'https://serene-atoll-56752.herokuapp.com'
 }
 
 // =============================
